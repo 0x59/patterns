eff37518f4e13edd92d04c66305c6ec9e75bad80
0x59 <0x59.sw@gmail.com>
2021-10-22 17:24:45 -0500

    add ring buffer and tests

---
+++ src/ringBuffer.js
@@ -0,0 +1,61 @@
+export class RingBuffer {
+
+  static #empty = Symbol()
+  static get empty() {
+    RingBuffer.#empty
+  }
+
+  #head
+  #queue
+  #size
+  #tail
+
+  constructor(size = -1) {
+    this.#head = 0
+    this.#queue = new Array(size).fill(RingBuffer.empty)
+    this.#size = size
+    this.#tail = 0
+  }
+
+  read = () => {
+    let item
+
+    if ((item = this.#read()) !== RingBuffer.empty) {
+      this.#clearTail()
+      this.#advanceTail()
+    }
+
+    return item
+  }
+
+  readAll = fn => {
+    let item
+
+    while ((item = this.#read()) !== RingBuffer.empty) {
+      this.#clearTail()
+      this.#advanceTail()
+      fn(item)
+    }
+  }
+
+  write = item => {
+    if (this.#isOverrun()) {
+      this.#advanceTail()
+    }
+    this.#write(item)
+    this.#advanceHead()
+  }
+
+  #advanceHead = () => this.#head = (this.#head + 1) % this.#size
+
+  #advanceTail = () => this.#tail = (this.#tail + 1) % this.#size
+
+  #clearTail = () => this.#queue[this.#tail] = RingBuffer.empty
+
+  #isOverrun = () => this.#queue[this.#head] !== RingBuffer.empty
+
+  #read = () => this.#queue[this.#tail]
+
+  #write = item => this.#queue[this.#head] = item
+
+}
---
+++ src/ringBuffer.test.js
@@ -0,0 +1,66 @@
+import * as Module from './ringBuffer'
+import {
+  RingBuffer
+} from './ringBuffer'
+import { jest } from '@jest/globals'
+
+describe('RingBuffer', () => {
+
+  it('exports correct module interface', () => {
+    const expected = {
+      RingBuffer: expect.any(Function)
+    }
+
+    expect(Object.keys(Module).sort()).toEqual(Object.keys(expected).sort())
+    expect(Module).toEqual(expect.objectContaining(expected))
+  })
+
+  describe('RingBuffer()', () => {
+
+    it('requires `size` argument', () => {
+      expect(() => new RingBuffer).toThrow()
+      expect(() => new RingBuffer(4)).not.toThrow()
+    })
+
+    it('has read-only empty value', () => {
+      expect(() => RingBuffer.empty = 'test').toThrow()
+    })
+
+    it('returns empty value when empty', () => {
+      const b = new RingBuffer(4)
+
+      expect(b.read()).toBe(RingBuffer.empty)
+    })
+
+    it('writes and reads one item', () => {
+      const b = new RingBuffer(4)
+      const testItem = {}
+
+      b.write(testItem)
+      expect(b.read()).toBe(testItem)
+    })
+
+    it('writes and reads `size` items', () => {
+      const b = new RingBuffer(4)
+      const testItems = [{}, {}, {}, {}]
+      let readItems = []
+
+      testItems.forEach(item => b.write(item))
+      b.readAll(item => readItems.push(item))
+
+      expect(readItems).toEqual(testItems)
+    })
+
+    it('reads last `size` items', () => {
+      const size = 4
+      const b = new RingBuffer(size)
+      const testItems = [{}, {}, {}, {}, {}, {}]
+      let readItems = []
+
+      testItems.forEach(item => b.write(item))
+      b.readAll(item => readItems.push(item))
+
+      expect(readItems).toEqual(testItems.slice(testItems.length - size))
+    })
+  })
+})
